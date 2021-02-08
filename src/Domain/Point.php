0a88318d175a1cf91ded5d5ce2288fede69904b6
Andrey Astakhov <andrey.astakhov@gmail.com>
2021-02-08 21:40:28 +0100

    Add point

--- src/Domain/Point.php
+++ src/Domain/Point.php
@@ -4,5 +4,24 @@ namespace App\Domain;
 
 class Point
 {
+    /**
+     * @var int
+     */
+    private $x;
 
+    /**
+     * @var int
+     */
+    private $y;
+
+    private function __construct(int $x, int $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public static function create(int $x, int $y)
+    {
+        return new self($x, $y);
+    }
 }
