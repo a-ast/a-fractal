1b7d0f127e5155a4f92206f47234be2372bf3e28
Andrey Astakhov <andrey.astakhov@gmail.com>
2021-02-12 23:15:12 +0100

    Implement a command that uses colour strategy.

--- src/Command/GenerateFileCommand.php
+++ src/Command/GenerateFileCommand.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Domain\Fractal\ColourStrategy;
+use App\Domain\Colour;
+use App\Domain\Fractal\PaletteColourStrategy;
 use App\Domain\Map;
 use App\Domain\Fractal\Config;
 use App\Domain\Fractal\JuliaSet;
+use App\Domain\Palette\GradientBuilder;
+use App\Domain\Palette\GradientConfig;
 use App\Infrastructure\Image;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -26,16 +29,24 @@ class GenerateFileCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $map = new Map(500, 500);
+        $fractalConfig = new Config();
+        $paletteConfig = new GradientConfig(Colour::fromRGB(0, 0, 0), Colour::fromRGB(0, 0, 0));
+        $paletteConfig->addInnerColour(Colour::fromRGB(255, 174, 0), 75);
+
+        $paletteBuilder = new GradientBuilder();
+        $palette = $paletteBuilder->build($fractalConfig->getMaxIterations()*10, $paletteConfig);
+
+        $map = new Map(1000, 1000);
 
         $fractal = new JuliaSet();
-        $config = new Config();
-        $fractal->build($config, $map);
+        $fractal->build($fractalConfig, $map);
 
-        $colourStrategy = new ColourStrategy($config);
+        $colourStrategy = new PaletteColourStrategy($fractalConfig, $palette);
 
         $image = new Image();
-        $image->saveToFile($map, 'var/z.png', $colourStrategy);
+
+        $image->savePaletteToFile($palette, 'var/palette.png');
+        $image->saveMapToFile($map, 'var/fractal.png', $colourStrategy);
 
         return Command::SUCCESS;;
     }
