39aa2838ff83f4f2f70312bf7d98b75d27cc9de7
Andrey Astakhov <andrey.astakhov@gmail.com>
2021-02-12 23:14:20 +0100

    Add saving palette to file.

--- src/Infrastructure/Image.php
+++ src/Infrastructure/Image.php
@@ -4,10 +4,11 @@ namespace App\Infrastructure;
 
 use App\Domain\Fractal\ColourStrategy;
 use App\Domain\Map;
+use App\Domain\Palette\Palette;
 
 class Image
 {
-    public function saveToFile(Map $map, string $fileName, ColourStrategy $colourStrategy)
+    public function saveMapToFile(Map $map, string $fileName, ColourStrategy $colourStrategy)
     {
         $file = imagecreatetruecolor($map->getWidth(), $map->getHeight());
 
@@ -26,6 +27,25 @@ class Image
                 $item->getX(),
                 $item->getY(),
                 $pixelColour);
+        }
+
+        imagepng($file, $fileName);
+    }
+
+    public function savePaletteToFile(Palette $palette, string $fileName)
+    {
+        $file = imagecreatetruecolor($palette->getSize(), 100);
+
+        /** @var \App\Domain\Colour $colour */
+        foreach ($palette->getColours() as $index => $colour) {
+
+            $pixelColour = imagecolorallocate($file,
+                $colour->getR(),
+                $colour->getG(),
+                $colour->getB()
+            );
+
+            imageline($file, $index, 0, $index, 99, $pixelColour);
         }
 
         imagepng($file, $fileName);
